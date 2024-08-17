29feda0aca28fcc931c1799ab9a4bac85601f2d1
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2024-08-17 20:27:59 +0530

    pipeline demo

--- terraform/dev/dev_app2/main.tf
+++ terraform/dev/dev_app2/main.tf
@@ -1,7 +1,14 @@
 resource "aws_iam_group" "group" {
-    name = "DemoGroup"
+    name = "DemoGroup1"
 }
 
 resource "aws_iam_user" "user" {
-    name = "DemoUser"
+    name = "DemoUser12"
+}
+terraform {
+    backend "s3" {
+        bucket = "mybucket-krish-new"
+        key    = "terraform.tfstate"
+        region = "us-east-1"
+    }
 }
